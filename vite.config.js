7347513324de43f5360370489bc35eb987f50c6f
Rafibarides <rafibaridesmusic@gmail.com>
2025-02-20 15:14:17 -0500

    fixing errors

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: '/src/main.jsx',
-    },
   },
   // Add any other necessary configurations here
 })
