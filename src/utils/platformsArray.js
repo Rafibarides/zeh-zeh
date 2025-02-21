d5ac44fa44d0144ea12c31d690bd4be577499b23
Rafibarides <rafibaridesmusic@gmail.com>
2025-02-21 16:42:24 -0500

    preview image

--- src/utils/platformsArray.js
+++ src/utils/platformsArray.js
@@ -2,16 +2,16 @@ export const platforms = [
   {
     icon: '/spotify.png',
     title: 'Spotify',
-    link: 'https://spotify.com/yourprofile'
+    link: 'https://open.spotify.com/artist/5Qh89NoKmcQw1EvGcxfIpZ?si=G-qVsunhR46BnlVKTFcuqw'
   },
   {
     icon: '/apple-music.png',
     title: 'Apple Music',
-    link: 'https://apple.com/music/yourprofile'
+    link: 'https://music.apple.com/us/artist/dovid-pearlman/992996914'
   },
   {
     icon: '/youtube.png',
     title: 'YouTube',
-    link: 'https://youtube.com/yourchannel'
+    link: 'https://www.youtube.com/@DovidPearlmanMusic/featured'
   }
 ];
