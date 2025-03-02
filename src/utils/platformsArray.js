313977c06a17fb5825f84ccf484291b6bb7b7265
Rafibarides <rafibaridesmusic@gmail.com>
2025-03-02 10:31:12 -0500

    updated links

--- src/utils/platformsArray.js
+++ src/utils/platformsArray.js
@@ -2,16 +2,21 @@ export const platforms = [
   {
     icon: '/spotify.png',
     title: 'Spotify',
-    link: 'https://open.spotify.com/artist/5Qh89NoKmcQw1EvGcxfIpZ?si=G-qVsunhR46BnlVKTFcuqw'
+    link: 'https://open.spotify.com/track/5yp1RiPqL5PIt4N46BLB2Y?si=86e9b1600ef74a7f'
   },
   {
     icon: '/apple-music.png',
     title: 'Apple Music',
-    link: 'https://music.apple.com/us/artist/dovid-pearlman/992996914'
+    link: 'https://music.apple.com/us/album/zeh-zeh/1798487751?i=1798487753'
   },
   {
     icon: '/youtube.png',
     title: 'YouTube',
-    link: 'https://www.youtube.com/@DovidPearlmanMusic/featured'
+    link: 'https://geo.music.apple.com/us/album/_/1798487751?i=1798487753&mt=1&app=music&ls=1&at=1000lHKX&ct=odesli_http&itscg=30200&itsct=odsl_m'
+  },
+  {
+    icon: '/24six.svg',
+    title: '24Six',
+    link: 'https://24six.app/preview/music/collection/12409/zeh-zeh'
   }
 ];
